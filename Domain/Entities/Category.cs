35b41b8df8aa349ad2846e0dca449fe7ba39b28a
kelvin.andrade <kelvin.andrade@DESKTOP-ANNE>
2023-03-26 11:11:49 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+        public Category(string name)
+        {
+            ValidateCategory(name);
+        }
+        public Category(int idCategory, string name)
+        {
+            DomainExceptionValidation.When(idCategory < 0, "Id não pode ser menor do que");
+            Id = idCategory;
+            ValidateCategory(name);
+        }
+
+        public ICollection<Product> Products { get; private set; }
+        private void Update(string name)
+        {
+            ValidateCategory(name);
+        }
+        private void ValidateCategory(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Nome inválido, o campo é obrigatório");
+            DomainExceptionValidation.When(name?.Length <3, "O tamtanho do nome não pode ser menor que 3");
+            Name = name;
+        }
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public sealed class Product : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateProduct(name, description, price, stock, image);
+        }
+        public Product(int idProduct, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(idProduct < 0, "IdProduto inválido, id precisa ser passado");
+            ValidateProduct(name, description, price, stock, image);
+            Id = idProduct;
+        }
+        public int IdCategory { get; set; }
+        public Category Category { get; set; }
+
+        private void UpdateProduct(string name, string description, decimal price, int stock, string image, int idCategory)
+        {
+            ValidateProduct(name, description, price, stock, image);
+            IdCategory = idCategory;
+        }
+        private void ValidateProduct(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Nome inválido, o campo é obrigatório");
+            DomainExceptionValidation.When(name?.Length < 3, "O tamtanho do nome não pode ser menor que 3");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Nome inválido, o campo é obrigatório");
+            DomainExceptionValidation.When(description?.Length < 6, "O tamtanho do nome não pode ser menor que 3");
+
+            DomainExceptionValidation.When(price < 0, "Id não pode ser menor do que");
+
+            DomainExceptionValidation.When(stock < 0, "Id não pode ser menor do que");
+
+            DomainExceptionValidation.When(image?.Length > 250, "O tamtanho do nome não pode ser menor que 3");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+    }
+}
